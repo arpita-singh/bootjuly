96361712e5c0dfff646b41db76721cdf45dff466
jhume <jordan.hume@learnosity.com>
2015-07-27 14:09:50 +1000

    Cleaned up report.php

--- report.php
+++ report.php
@@ -48,100 +48,96 @@
     $Init = new Init($service, $security, $secret, $request);
 
     // Call the generate() method to retrieve a JavaScript object
-    $Request = $Init->generate();
+    $initOptions = $Init->generate();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Movie Trivia Reporting</title>
-    <script src="//reports.learnosity.com"></script>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
+        <title>Movie Trivia Reporting</title>
+        <script src="//reports.learnosity.com"></script>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
 
-  </head>
-  <body>
+    </head>
+    <body>
 
     <div class="container">
-	<div class="row text-center">
-		<div class="col-md-12 text-center">
-			<h3>
-				MOVIE TRIVIA - Leader Board
-			</h3>
-			<table class="table">
-				<thead>
-					<tr>
-						<th class="text-center">
-							Rank
-						</th>
-						<th class="text-center">
-							UserName
-						</th>
-						<th class="text-center">
-							Score
-						</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>
-							1
-						</td>
-						<td>
-							Jordan
-						</td>
+        <div class="row text-center">
+            <div class="col-md-12 text-center">
+                <h3>
+                    MOVIE TRIVIA - Leader Board
+                </h3>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th class="text-center">
+                                Rank
+                            </th>
+                            <th class="text-center">
+                                UserName
+                            </th>
+                            <th class="text-center">
+                                Score
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                1
+                            </td>
+                            <td>
+                                Jordan
+                            </td>
 
-						<td>
-							15/15
-						</td>
-					</tr>
-					<tr class="active">
-						<td>
-							2
-						</td>
-						<td>
-						Brody
-						</td>
+                            <td>
+                                15/15
+                            </td>
+                        </tr>
+                        <tr class="active">
+                            <td>
+                                2
+                            </td>
+                            <td>
+                            Brody
+                            </td>
 
-						<td>
-							12/15
-						</td>
-					</tr>
-					<tr class="success">
-						<td>
-							3
-						</td>
-						<td>
-							Dennis
-						</td>
+                            <td>
+                                12/15
+                            </td>
+                        </tr>
+                        <tr class="success">
+                            <td>
+                                3
+                            </td>
+                            <td>
+                                Dennis
+                            </td>
 
-						<td>
-							5/15
-						</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
-	</div>
-</div>
+                            <td>
+                                5/15
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 
-<div style="padding-bottom:30px;" class="container text-center"><a href="/bootjuly"><button id="backLogin">Back to login page</button></a></div>
-
-<span class="learnosity-report" id="report-1"></span>
-
-<span class="learnosity-report" id="report-2"></span>
-
-
-
-    <script>
-      var reportsApp = LearnosityReports.init(<?php echo($Request) ?>);
-    </script>
-
+    <div style="padding-bottom:30px;" class="container text-center"><a href="/bootjuly"><button id="backLogin">Back to login page</button></a></div>
+    <span class="learnosity-report" id="report-1"></span>
+    <span class="learnosity-report" id="report-2"></span>
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
+    <script>
+        var initOptions = <?php echo($initOptions) ?>;
+
+        var reportsApp = LearnosityReports.init(initOptions);
+        </script>
   </body>
 </html>
